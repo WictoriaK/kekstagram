628df7a254000cbd976fbf8e87cd19d038939791
Victoria <vitagood.92@list.ru>
2025-07-23 21:42:50 +0300

    completed module-7.11

--- js/photos.js
+++ js/photos.js
@@ -1,13 +1,20 @@
+import {showBigPhoto} from './fullPhoto.js';
+
 const photoListElement =  document.querySelector('.pictures');
 const photoTemplateElement = document.querySelector('#picture').content.querySelector('.picture');
 
-// клонируем щ=шаблон фото и заполняет данными
-const createPhotos = (({url, likes, comments}) => {
+// клонируем шаблон фото и заполняет данными
+const createPhotos = ((data) => {
+  const {url, likes, comments} = data;
   const photoElement = photoTemplateElement.cloneNode(true);
 
   photoElement.querySelector('.picture__img').src = url;
   photoElement.querySelector('.picture__likes').textContent = likes;
   photoElement.querySelector('.picture__comments').textContent = comments.length;
+
+  photoElement.addEventListener('click', () => {
+    showBigPhoto(data);
+  });
 
   return photoElement;
 });
